ea52e162adfbd9531170d01d13e179b865bd6114
Cliff <cliffxzx@gmail.com>
2020-06-08 03:46:12 +0800

    camera calibrating

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/ARTagUtils.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/ARTagUtils.java
@@ -28,15 +28,18 @@ public class ARTagUtils {
             try {
                 List<Mat> corners = new ArrayList<>();
                 Mat ids = new Mat();
-                Aruco.detectMarkers(api.getMatNavCam(), dictionary, corners, ids);
-                res = Integer.toString((int)ids.get(0, 0)[0]);
-                Mat camaraMatrix = new Mat(3, 3, CvType.CV_64FC4);
-                camaraMatrix.put(3, 3, 344.173397, 0, 630.793795, 0, 344.277922, 487.033834, 0, 0, 1);
-                Mat distCoeffs = new Mat(1, 5, CvType.CV_64FC4);
-                distCoeffs.put(1, 5, -0.152963, 0.017530, -0.001107, -0.000210, 0);
+                Aruco.detectMarkers(img, dictionary, corners, ids);
+                double cameraMat[][] = { { 344.173397, 0, 630.793795 }, { 0, 344.277922, 487.033834}, { 0, 0, 1 } };
+                Mat cameraMatrix = new Mat(3, 3, CvType.CV_64F);
+                for(int w = 0; w < 3; ++w)
+                    cameraMatrix.put(w, 0, cameraMat[w]);
+
+                double distCoeffMat[] = { -0.152963, 0.017530, -0.001107, -0.000210, 0 };
+                Mat distCoeffs = new Mat(1, 5, CvType.CV_64F);
+                distCoeffs.put(0, 0, distCoeffMat);
 
                 Mat rvecs = new Mat(), tvecs = new Mat();
-                Aruco.estimatePoseSingleMarkers(corners, 5, camaraMatrix, distCoeffs, rvecs, tvecs);
+                Aruco.estimatePoseSingleMarkers(corners, 5, cameraMatrix, distCoeffs, rvecs, tvecs);
 
                 Log.d("Seal", res);
                 api.judgeSendDiscoveredAR(res);
